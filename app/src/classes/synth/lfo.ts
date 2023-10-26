721578acaca6be9505cce287baa5118a04f391c5
Sebastian Wiendlocha <sebastian@wiendlocha.org>
2023-10-26 22:50:34 +0200

    Change lfo range from (0, 1) to (-1, 1)

--- app/src/classes/synth/lfo.ts
+++ app/src/classes/synth/lfo.ts
@@ -16,7 +16,7 @@ export default class LFO {
   private lforateInputScale: Tone.Scale;
 
   constructor(frequency?: number, min?: number, max?: number) {
-    this.lfo = new Tone.LFO(frequency ?? 0.7, min ?? 0, max ?? 1);
+    this.lfo = new Tone.LFO(frequency ?? 0.7, min ?? -1, max ?? 1);
 
     this.output = new Tone.Scale(0, 1);
     this.lfo.connect(this.output);
